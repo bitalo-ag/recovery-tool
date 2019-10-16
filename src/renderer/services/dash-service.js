afae3da4da8e2c13a49ab6fd95810bf17a851c51
Ihor Savva <savva.ihor@gmail.com>
2019-10-16 18:01:30 +0300

    Add dash service

--- src/renderer/services/dash-service.js
+++ src/renderer/services/dash-service.js
@@ -13,31 +13,15 @@ var bitcoin = require('bitcoinjs-lib-dash')
 
 var config = require('../config').config
 
-bitcore.Networks.defaultNetwork = config.network == 'testnet' ? bitcore.Networks.testnet : bitcore.Networks.livenet;
-var network = config.network == 'testnet' ?  bitcoin.networks.testnet :  bitcoin.networks.bitcoin
+bitcore.Networks.defaultNetwork = bitcore.Networks.livenet
+var network = bitcoin.networks.dash
 
 export default {
   signHex: function (data) {
-    var key = bitcoin.ECPair.fromWIF(bitcore.PrivateKey.fromString(data['master_key']).toWIF())
-    var tx = bitcoin.TransactionBuilder.fromTransaction(bitcoin.Transaction.fromHex(data['hex']));
+    var key = bitcoin.ECPair.fromWIF(bitcore.PrivateKey.fromString(data['master_key']).toWIF(), network)
+    var tx = bitcoin.TransactionBuilder.fromTransaction(bitcoin.Transaction.fromHex(data['hex']), network);
     data['utxos'].forEach((utxo, i)=>{
-      const pubkeys = utxo['public_keys'].map((pubkey) => new Buffer(pubkey, 'hex'))
-      const p2ms = bitcoin.payments.p2ms({ m: 2, pubkeys, network: network })
-      const p2wsh = bitcoin.payments.p2wsh({ redeem: p2ms, network: network })
-      const p2sh = bitcoin.payments.p2sh({ redeem: p2wsh, network: network })
-      if (tx.__inputs[0].signatures){
-        Object.assign(tx.__inputs[0], {signType: "multisig", value: params.utxo.amount.toSatoshi(), signScript: bitcore.Script.buildMultisigOut(utxo['public_keys'].sort(), 2).toBuffer()})
-        var signature = tx.__inputs[i].signatures[0]
-        tx.__inputs[0].signatures = [undefined,undefined]
-        utxo['public_keys'].sort().forEach((pkey, i)=> {
-          if(pkey =! data['public_key']){
-            tx.__inputs[0].signatures[i] = signature
-          }else{
-            tx.__inputs[0].signatures[i] = undefined
-          }
-        })
-      }
-      tx.sign(i, key, p2sh.redeem.output, null, (+utxo['amount']).toSatoshi(), p2wsh.redeem.output)
+      tx.sign(i, key)
     })
     return tx.build().toHex()
   },
